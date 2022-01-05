bda30eaec43648f9a159724cab348ae405cfed10
ravina <ravinahazari11@gmail.com>
2022-01-05 17:55:46 +0530

    Simple.java

---
+++ Simple.java
@@ -0,0 +1,11 @@
+public class Simple
+{
+public static void main(String[] args)
+{
+System.out.println("Hello Java");
+System.out.println(2+3);
+System.out.println(3-2);
+System.out.println(3*2);
+System.out.println(4/2);
+}
+}
